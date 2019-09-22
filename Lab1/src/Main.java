8759c4c0359184efd367b2e82fb4215a3d12084b
Kevin Wang <wh4wang@uwaterloo.ca>
2019-09-22 17:03:06 -0400

    finished lab

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,12 +1,43 @@
+import Queues.MM1KQueue;
 import Queues.QueueEvent;
 import Utils.ExponentialRandomVariable;
 import Queues.MM1Queue;
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        question1();
-        MM1(125, 0.0005,1000000, 1000);
+        Scanner scanner = new Scanner(System.in);
+        double averagePacketLength = 2000;
+        double averageTransmissonRate = 1000000;
+        System.out.print("For Question 1 enter 1, for MM1 enter 2, for MM1K enter 3.");
+        String option = scanner.next();
+
+        switch (option) {
+            case "1": {
+                question1();
+                break;
+            }
+            case "2": {
+                System.out.print("Enter the total simulation time:");
+                double simulationTime  = scanner.nextDouble();
+                System.out.print("Enter the value of rou for this simulation:");
+                double rho  = scanner.nextDouble();
+                double arrivalRate = (rho * averageTransmissonRate) / averagePacketLength;
+                MM1(arrivalRate, 1/averagePacketLength, averageTransmissonRate, simulationTime);
+                break;
+            }
+            case "3": {
+                System.out.print("Enter the total simulation time:");
+                double simulationTime  = scanner.nextDouble();
+                System.out.print("Enter the value of rou for this simulation:");
+                double rho  = scanner.nextDouble();
+                System.out.print("Enter the size of queue:");
+                int queueSize  = scanner.nextInt();
+                double arrivalRate = (rho * averageTransmissonRate) / averagePacketLength;
+                MM1K(arrivalRate, 1/averagePacketLength, averageTransmissonRate, simulationTime, queueSize);
+                break;
+            }
+        }
     }
 
     public static void question1() {
@@ -27,4 +58,12 @@ public class Main {
         mm1Queue.startSimulation();
 
     }
+
+    public static void MM1K(double packetsPerSecond, double packetLength, double transmissonRate, double totalSimulationTime, int queueSize) {
+        MM1KQueue mm1kQueue = new MM1KQueue(packetsPerSecond, packetLength, transmissonRate, totalSimulationTime, queueSize);
+        mm1kQueue.addEvents();
+        mm1kQueue.startSimulation();
+    }
+
+
 }
--- Lab1/src/Queues/MM1KQueue.java
+++ Lab1/src/Queues/MM1KQueue.java
@@ -1,40 +1,47 @@
 package Queues;
 
+import Utils.ExponentialRandomVariable;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
-import Utils.ExponentialRandomVariable;
 
-public class MM1Queue {
+public class MM1KQueue {
     private final double arrivalRate;
     private final double observerRate;
     private final double packetLength;
     private final double totalSimulationTime;
     private final double transmissionRate;
 
-    public final LinkedList<QueueEvent> queue = new LinkedList<>();
 
-    // Used for collecting data
-    private int queueSize = 0;
+    public final LinkedList<QueueEvent> eventsQueue = new LinkedList<>();
+    private final LinkedList<QueueEvent> packetQueue = new LinkedList<>();
+
+    private final int totalQueueSize;
+    private int currentQueueSize = 0;
+    private int packetsDropped = 0;
     private int numberOfObserverEvents = 0;
     private int totalNumberofPacketsObserved = 0;
+    private int queueSize = 0;
+    private int simulationQueueSize = 0;
     private int idleCounter = 0;
     private int generatedPackets = 0;
     private int numberOfArrivals = 0;
     private int numberOfDepartures = 0;
 
-    public MM1Queue(double arrivalRate, double packetLength, double transmissionRate, double totalSimulationTime){
+    public MM1KQueue(double arrivalRate, double packetLength, double transmissionRate, double totalSimulationTime, int queueSize){
         this.arrivalRate = arrivalRate;
         this.observerRate = this.arrivalRate * 5;
         this.totalSimulationTime = totalSimulationTime;
         this.packetLength = packetLength;
         this.transmissionRate = transmissionRate;
+        this.totalQueueSize = queueSize;
     }
 
     public void addEvents() {
         createArrivalEvents();
         createObserverEvents();
-        Collections.sort(this.queue, new Comparator<QueueEvent>() {
+        Collections.sort(this.eventsQueue, new Comparator<QueueEvent>() {
             @Override
             public int compare(QueueEvent o1, QueueEvent o2) {
                 if (o1.eventTime - o2.eventTime > 0) {
@@ -53,12 +60,21 @@ public class MM1Queue {
         while (time < this.totalSimulationTime) {
             arrival = ExponentialRandomVariable.generateRandomVariable(this.arrivalRate);
             time += arrival;
-            currentDeparture = createDepartureEvents(previousDeparture, time);
-            QueueEvent arrivalEvent = new QueueEvent(QueueEvent.Type.Arrival, time);
-            queue.add(arrivalEvent);
-            queue.add(currentDeparture);
-            previousDeparture = currentDeparture;
+            removePacketFromQueue(time);
+            if (this.packetQueue.size() == this.totalQueueSize) {
+                this.packetsDropped += 1;
+                System.out.println("packet dropped");
+            } else {
+                currentDeparture = createDepartureEvents(previousDeparture, time);
+                QueueEvent arrivalEvent = new QueueEvent(QueueEvent.Type.Arrival, time);
+                this.eventsQueue.add(arrivalEvent);
+                this.eventsQueue.add(currentDeparture);
+                this.packetQueue.add(currentDeparture);
+                this.currentQueueSize += 1;
+                previousDeparture = currentDeparture;
+            }
         }
+        this.generatedPackets += 1;
     }
 
     private QueueEvent createDepartureEvents(QueueEvent previousEvent, double arrivalTime) {
@@ -74,6 +90,16 @@ public class MM1Queue {
         return departureEvent;
     }
 
+    private void removePacketFromQueue(double currentTime) {
+        if (this.packetQueue.size() == 0 || this.packetQueue.getFirst().eventTime > currentTime) {
+            return;
+        }
+
+        while(this.packetQueue.size() > 0  && this.packetQueue.getFirst().eventTime <= currentTime) {
+            this.packetQueue.removeFirst();
+        }
+    }
+
     private void createObserverEvents() {
         double time = 0;
         double arrival;
@@ -81,36 +107,32 @@ public class MM1Queue {
             arrival = ExponentialRandomVariable.generateRandomVariable(this.observerRate);
             time += arrival;
             QueueEvent observerEvent = new QueueEvent(QueueEvent.Type.Observer, time);
-            queue.add(observerEvent);
+            this.eventsQueue.add(observerEvent);
         }
     }
 
     private void handleObserverEvent() {
         this.numberOfObserverEvents += 1;
-        this.totalNumberofPacketsObserved += this.queueSize;
-        if (this.queueSize == 0) {
+        this.totalNumberofPacketsObserved += this.simulationQueueSize;
+        if (this.simulationQueueSize == 0) {
             this.idleCounter += 1;
         }
     }
 
     private void handleArrivalEvent() {
-        this.queueSize += 1;
-        this.generatedPackets += 1;
+        this.simulationQueueSize += 1;
         this.numberOfArrivals += 1;
     }
 
     private void handleDepartureEvent() {
-        this.queueSize -= 1;
+        this.simulationQueueSize -= 1;
         this.numberOfDepartures += 1;
     }
 
     public void startSimulation() {
         QueueEvent currentEvent;
-        while (this.queue.size() > 0) {
-            if (this.queueSize < 0) {
-                System.out.println("queue size is below 0");
-            }
-            currentEvent = this.queue.removeFirst();
+        while (this.eventsQueue.size() > 0) {
+            currentEvent = this.eventsQueue.removeFirst();
             if (currentEvent.queueType == QueueEvent.Type.Observer) {
                 handleObserverEvent();
             } else if (currentEvent.queueType == QueueEvent.Type.Arrival) {
@@ -121,10 +143,9 @@ public class MM1Queue {
         }
         double expectedValue = Double.valueOf(this.totalNumberofPacketsObserved)/Double.valueOf(this.numberOfObserverEvents);
         System.out.println(String.format(
-                "Number of Arrivals: %s, Number of Departures: %s, Number of Observartions: %s, Idle Counter: %s, Number of Generated Packets: %s, Total packets in Queue: %s, Expected Value: %s",
-                this.numberOfArrivals, this.numberOfDepartures, this.numberOfObserverEvents, this.idleCounter, this.generatedPackets, this.totalNumberofPacketsObserved, expectedValue)
+                "Number of Arrivals: %s, Number of Departures: %s, Number of Observartions: %s, Idle Counter: %s, Number of Generated Packets: %s, Total packets in Queue: %s, Total packets dropped: %s, Expected Value: %s",
+                this.numberOfArrivals, this.numberOfDepartures, this.numberOfObserverEvents, this.idleCounter, this.generatedPackets, this.totalNumberofPacketsObserved, this.packetsDropped, expectedValue)
         );
     }
-
 
 }
